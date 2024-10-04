7849e5b6b1f8e576208ea4c38b75e706e80cf8a9
JorgeSaicoski <jorgesaicoski@gmail.com>
2024-10-04 16:41:12 -0300

    Structure and StartTotalTime

--- app.go
+++ app.go
@@ -3,12 +3,60 @@ package main
 import (
 	"context"
 	"time"
-
-	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
+type TotalTime struct {
+	StartTime  time.Time
+	FinishTime time.Time
+	WorkTimes  []*WorkTime
+	BreakTime  *BreakTime
+}
+
+type Timer struct {
+	Time    time.Time
+	Message string
+}
+
+type WorkTime struct {
+	StartTime time.Time
+	Duration  time.Duration
+	Projects  []Project
+	Brb       *Brb
+}
+
+type BreakTime struct {
+	StartTime time.Time
+	Duration  time.Duration
+}
+
+type Project struct {
+	StartTime time.Time
+	Duration  time.Duration
+	Cost      *Cost
+	WorkTimes []WorkTime
+	Tasks     []Task
+}
+
+type Task struct {
+	Project     Project
+	Deadline    time.Time
+	Description string
+}
+
+type Cost struct {
+	Time     time.Time
+	HourCost int
+}
+
+type Brb struct {
+	StartTime time.Time
+	Duration  time.Duration
+}
+
 type App struct {
-	ctx context.Context
+	ctx       context.Context
+	totalTime *TotalTime
+	timer     *Timer
 }
 
 func NewApp() *App {
@@ -19,16 +67,11 @@ func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-func (a *App) TestMessage() string {
-	return "Test"
-}
-
-func (a *App) StartTimer(seconds int) {
-	time.Sleep(time.Duration(seconds) * time.Second)
-
-	runtime.MessageDialog(a.ctx, runtime.MessageDialogOptions{
-		Type:    runtime.InfoDialog,
-		Title:   "Timer",
-		Message: "Time finish",
-	})
+func (a *App) StartTotalTime() string {
+	a.totalTime = &TotalTime{
+		StartTime: time.Now(),
+		WorkTimes: []*WorkTime{},
+		BreakTime: &BreakTime{},
+	}
+	return "Day initialized. Have a good journey!"
 }
