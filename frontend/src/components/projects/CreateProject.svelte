06ee0427b836497daaf835912d6f53d72fff9ea9
JorgeSaicoski <jorgesaicoski@gmail.com>
2024-10-21 14:48:45 -0300

    Create Project with associate option

--- frontend/src/components/projects/CreateProject.svelte
+++ frontend/src/components/projects/CreateProject.svelte
@@ -1,8 +1,9 @@
 <script>
-    import { CreateProject } from "../../../wailsjs/go/main/App";
+    import { CreateProject, AssociateProjectToWorkTime } from "../../../wailsjs/go/main/App";
     import { createEventDispatcher } from 'svelte';
 
     const dispatch = createEventDispatcher()
+    let associateProject = false
     let projectName = "";
     let message = null;
     let project = null;
@@ -13,6 +14,10 @@
             message = response.message
             project = response.project
             projectName = ""
+            if (associateProject) {
+                await AssociateProjectToWorkTime(project.ID);
+                message += ` Project associated with the current work time.`;
+            }
             
         } catch (error) {
             message = error.message
@@ -40,6 +45,15 @@
       bind:value={projectName}
     ></textarea>
     <p class="mt-2 text-sm text-gray-500">You entered: {projectName}</p>
+    <div class="flex items-center">
+        <input 
+          type="checkbox" 
+          id="associateProjectCheckbox" 
+          class="mr-2"
+          bind:checked={associateProject}
+        />
+        <label for="associateProjectCheckbox" class="text-sm">Associate project with current work time</label>
+    </div>
     <div class="flex justify-center">
         <button
           class="px-4 py-2 bg-sky-600 text-white rounded-md hover:bg-sky-700 focus:outline-none focus:ring-2 focus:ring-sky-500"
@@ -49,11 +63,13 @@
         </button>
       </div>
     {#if project}
-        <div class="w-full max-w-2xl p-4 space-y-0 bg-teal-400 rounded shadow-lg text-black">Project {project.Name} created with id {project.ID}. And associated with the currently worktime.</div>
+        <div class="w-full max-w-2xl p-4 space-y-0 bg-teal-400 rounded shadow-lg text-black">
+            Project {project.Name} created with id {project.ID}.
+        </div>
         <button 
         on:click={returnToWorkTime} 
         class="flex-1 py-3 px-6 bg-blue-600 hover:bg-blue-700 text-white rounded-md transition-colors duration-200 ease-in-out shadow-md">
         Return to Work Time
-    </button>
+        </button>
     {/if}
 </div>
